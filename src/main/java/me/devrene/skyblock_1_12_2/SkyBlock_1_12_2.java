686cc811b7964a40e4bb801111929bd79bffd725
bucketgames-code <litebucket@gmail.com>
2022-12-11 00:34:27 +0100

    Pushing default Project-Files

---
+++ src/main/java/me/devrene/skyblock_1_12_2/SkyBlock_1_12_2.java
@@ -0,0 +1,17 @@
+package me.devrene.skyblock_1_12_2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyBlock_1_12_2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
